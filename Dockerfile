b2ba6c51c863389e14ea8dcf25a0e2cbf9dd6e87
taka6745 <takamundy@gmail.com>
2023-07-05 22:05:30 +1000

    Built for portfolio

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,24 @@
-FROM node:14
+# Use the official Node.js image as the base, for arm64 architecture
+FROM --platform=linux/arm64/v8 node
 
-WORKDIR /usr/src/app
+# Set the working directory inside the container
+WORKDIR /app
 
+# Copy package.json and package-lock.json to the container
 COPY package*.json ./
 
+# Install dependencies
 RUN npm install
 
+# Copy the rest of the application code to the container
 COPY . .
 
-EXPOSE 3030
+# Install Certbot
+RUN apt-get update && apt-get install -y certbot
 
+# Expose the ports for HTTP and HTTPS
+EXPOSE 80
+EXPOSE 443
+
+# Run the Node.js server
 CMD [ "node", "src/server.js" ]
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const updateRoutes = require('./routes/update');
 const Knex = require('knex');
 const knexConfig = require('../knexfile');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -30,6 +33,23 @@ knex.migrate.latest()
 app.use('/git', gitRoutes);
 app.use('/update', updateRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// Check if SSL certificate files exist
+const sslCertExists = fs.existsSync('/etc/letsencrypt/live/api.takodamundy.tech/fullchain.pem');
+const sslKeyExists = fs.existsSync('/etc/letsencrypt/live/api.takodamundy.tech/privkey.pem');
+
+if (sslCertExists && sslKeyExists) {
+  // SSL certificate files exist, start HTTPS server
+  const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.takodamundy.tech/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/api.takodamundy.tech/privkey.pem')
+  };
+
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`HTTPS server listening on port ${PORT}`);
+  });
+} else {
+  // SSL certificate files do not exist, start HTTP server
+  http.createServer(app).listen(PORT, () => {
+    console.log(`HTTP server listening on port ${PORT}`);
+  });
+}
